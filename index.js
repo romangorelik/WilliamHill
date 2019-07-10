78025ff38513b4df419cf6a6cfcff3dcc7cfed8d
Roman Gorelik <romang31@gmail.com>
2019-07-10 19:21:30 -0400

    Added more support for server using different middleware

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 let app = express()
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(express.static(__dirname + '/client/dist/'))
+
+app.get('/working', (req, res) => {
+  res.send('Server working')
+})
 
 app.use(express.static(__dirname + '/client/dist/'))
 
